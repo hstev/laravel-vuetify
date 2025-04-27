e3ddc7f74f95ef0819566bd332a7fef74373217c
Harol Restrepo <hstevdev@gmail.com>
2025-04-27 11:28:22 -0500

    feat: Dashboard page

---
+++ resources/js/pages/Dashboard.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { Head, Link, router } from '@inertiajs/vue3';
+const handleLogout = () => {
+    router.flushAll();
+};
+</script>
+<template>
+    <Head title="Dashboard" />
+    <div>
+        <h1>Dashboard</h1>
+        <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
+            <LogOut class="mr-2 h-4 w-4" />
+            Log out
+        </Link>
+    </div>
+</template>
