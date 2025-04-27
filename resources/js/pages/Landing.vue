1e58aeb9ec49ae4b0b9c2437904744db0b8c2fb0
Harol Restrepo <hstevdev@gmail.com>
2025-04-26 22:00:36 -0500

    fix: landing

--- resources/js/pages/Landing.vue
+++ resources/js/pages/Landing.vue
@@ -4,13 +4,24 @@ import { Head } from '@inertiajs/vue3';
 <template>
     <Head title="Landing" />
     <div class="container-landing">
-        <span class="laravel">Laravel</span> 🤝 <span class="vuetify">Vuetify</span>
+        <v-card
+            append-icon="mdi-open-in-new"
+            class="mx-auto"
+            href="https://github.com/hstev/laravel-vuetify/"
+            max-width="344"
+            prepend-icon="mdi-github"
+            rel="noopener"
+            subtitle="Github repository"
+            target="_blank"
+            title="hstev/laravel-vuetify"
+        ></v-card>
     </div>
 </template>
 
 <style scoped>
 .container-landing {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100vh;
